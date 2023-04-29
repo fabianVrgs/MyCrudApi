a67e9da1651226a31741c233dc4f266518be1ea2
fabianVrgs <125383255+fabianVrgs@users.noreply.github.com>
2023-04-29 18:31:04 -0500

    Add project files.

---
+++ MyCrudApi/Controllers/UserController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using MyCrudApi.Data;
+using MyCrudApi.models;
+
+namespace MyCrudApi.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class UserController : Controller
+	{
+		private readonly MyCrudContext _context;
+		public UserController(MyCrudContext context)
+		{
+			_context = context;
+		}
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			return Json(await _context.User.ToArrayAsync());
+		}
+		
+		[HttpGet("{id}")]
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null || _context.User == null)
+			{
+				return NotFound();
+			}
+
+			var user = await _context.User.
+				FirstOrDefaultAsync(m =>m.Id == id);
+			if(user == null)
+			{
+				return NotFound();
+			}
+				return Json(user);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Email,Password")] User user)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Add(user);
+				await _context.SaveChangesAsync();
+				return Json(user);
+			}
+			return Json(user);
+		}
+		[HttpPatch("{id}")]
+		public async Task<IActionResult> Update(int? id, [FromBody] User user)
+		{
+			if (id != user.Id)
+			{
+				return BadRequest();
+			}
+
+			_context.Entry(user).State = EntityState.Modified;
+			await _context.SaveChangesAsync();
+			return Json (user);		
+		}
+		
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> Delete(int id)
+		{
+
+			var user = await _context.User.FindAsync(id);
+			if (user == null)
+			{
+				return NotFound();
+			}
+
+			_context.User.Remove(user);
+			await _context.SaveChangesAsync();
+
+			return NoContent();
+		}
+	}
+}
---
+++ MyCrudApi/Data/MyCrudContext.cs
@@ -0,0 +1,23 @@
+﻿using MyCrudApi.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyCrudApi.Data
+{
+	public class MyCrudContext : DbContext
+	{
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			
+			builder.Entity<User>(entity =>
+			{
+				entity.ToTable("user");
+			});
+		}
+		public MyCrudContext(DbContextOptions<MyCrudContext> options) 
+		: base(options) 
+		{
+		}
+
+		public DbSet<User> User { get; set; } = null!;
+	}
+}
